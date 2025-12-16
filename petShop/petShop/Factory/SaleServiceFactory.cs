ed1066bb4b0b452b307eef5f5d5a7c3daa96bcb0
Danica Todorovic <148051191+danicaklaun@users.noreply.github.com>
2025-12-16 12:03:13 +0100

    novi folder

---
+++ petShop/petShop/Factory/SaleServiceFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using petShop.Services;
+using petShop.Model;
+
+public static class SalesServiceFactory
+{
+    public static ISalesService Create(List<Receipt> receipts)
+    {
+        var now = DateTime.Now.TimeOfDay;
+
+        if (now >= new TimeSpan(8, 0, 0) && now < new TimeSpan(16, 0, 0))
+            return new DaySalesService(receipts);
+
+        if (now >= new TimeSpan(16, 0, 0) && now < new TimeSpan(22, 0, 0))
+            return new NightSalesService(receipts);
+
+        throw new InvalidOperationException("Store is closed.");
+    }
+}
---
+++ petShop/petShop/Services/NightSaleService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using petShop.Model;
+using petShop.Services;
+
+public class NightSalesService : ISalesService
+{
+    private readonly List<Receipt> receipts;
+
+    public NightSalesService(List<Receipt> receipts)
+    {
+        this.receipts = receipts;
+    }
+
+    public IReadOnlyCollection<Receipt> GetAllReceipts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Receipt SellPet(Pet pet)
+    {
+        if (Session.CurrentUser == null || Session.CurrentUser.Role != Role.Seller)
+            throw new UnauthorizedAccessException();
+
+        pet.MarkAsSold();
+
+        int taxedAmount = (int)(pet.SellingPrice * 1.10);
+
+        var receipt = new Receipt(Session.CurrentUser, taxedAmount);
+        receipts.Add(receipt);
+
+        return receipt;
+    }
+
+    Receipt ISalesService.SellPet(Pet pet)
+    {
+        throw new NotImplementedException();
+    }
+}
